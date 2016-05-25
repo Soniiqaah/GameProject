48b43412a4590ea3d408b3db44ae9e616a5824a0
Jannike <Jannike@rymden.nu>
2016-05-25 21:34:05 +0200

    Ball.java, FirstStage.java
    Ball: klass för boll - starta bollens rörelse, bollen stannar
    FirstStage: skapar en boll, lägg till bollen på planen, klicka på planen
    för att styra riktning och fart

--- application/Ball.java
+++ application/Ball.java
@@ -1,34 +1,69 @@
 package application;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.scene.Node;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 
 class Ball extends Circle {
 	private double dx = 1, dy = 1;
 	private double paneWidth, paneHeight;
+	public boolean ballStopped = false;
+	private Timeline animation;
 
-	Ball(double x, double y, double radius, Color color, double paneWidth, double paneHeight) {
+	Ball(double x, double y, double radius, Color color, double paneWidth,
+			double paneHeight, double dx, double dy) {
 		super(x, y, radius);
 		this.paneWidth = paneWidth;
 		this.paneHeight = paneHeight;
+		this.dx = dx;
+		this.dy = dy;
 		setFill(color); // Set ball color
 	}
 
 	protected void moveBall() {
 		// Check boundaries
 		// this inte nödvändigt men förtydligande i sammanhanget TODO
-		if (this.getCenterX() < this.getRadius() || this.getCenterX() > this.paneWidth - this.getRadius()) {
-			this.dx *= -1; // Change ball move direction
+		if ((this.getCenterX() < this.getRadius()) && (this.dx < 0)) {
+			this.dx *= -1; // change direction when hitting the left wall
+		} else if ((this.getCenterX() > this.paneWidth - this.getRadius())
+				&& (this.dx > 0)) {
+			this.dx *= -1; // change direction when hitting the right wall
 		}
-		if (this.getCenterY() < this.getRadius() || this.getCenterY() > this.paneHeight - this.getRadius()) {
-			this.dy *= -1; // Change ball move direction
+		if ((this.getCenterY() < this.getRadius()) && (this.dy < 0)) {
+			this.dy *= -1; // change direction when hitting the top wall
+		} else if ((this.getCenterY() > this.paneHeight - this.getRadius())
+				&& (this.dy > 0)) {
+			this.dy *= -1; // change direction when hitting the bottom wall
 		}
 
-		// Adjust ball position
+		// Ball movement
 		this.setCenterX(this.dx + this.getCenterX());
 		this.setCenterY(this.dy + this.getCenterY());
-		this.dx *= 0.999; // inte original
-		this.dy *= 0.999; // inte original
+		// Decreasing the speed
+		this.dx *= 0.99;
+		this.dy *= 0.99;
+		// Make the ball stop a bit quicker
+		if ((Math.abs(this.dx) < 0.005) && (Math.abs(this.dy) < 0.005)) {
+			this.dx = 0;
+			this.dy = 0;
+		}
+		// The ball has stopped
+		if ((this.dx == 0) && (this.dy == 0)) {
+			ballStopped = true;
+			animation.stop(); // Stop animation
+		}
 	}
+
+	// The animation of the balls movement
+	public void animateBallMovement() {
+		animation = new Timeline(
+				new KeyFrame(Duration.millis(50), e -> moveBall()));
+		animation.setCycleCount(Timeline.INDEFINITE);
+		animation.play(); // Start animation
+
+	}
+
 }
--- application/FirstStage.java
+++ application/FirstStage.java
@@ -1,8 +1,12 @@
 package application;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
@@ -10,74 +14,75 @@ import javafx.scene.shape.LineTo;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class FirstStage extends Application {
 
 	double xcoor;
 	double ycoor;
+	Ball ball;
+	double clickedXCoord; //måste kommas åt i eventhandlern
+	double clickedYCoord;
+	int startXCoord;
+	int startYCoord;
+	double distanceX;
+	double distanceY;
 
 	public void start(Stage primaryStage) {
 
 		BorderPane pane = new BorderPane();
-		Label resultLabel = new Label();
 		Path path = new Path();
-		int startXCoord;
-		int startYCoord;
 		int playingfieldWidth; // ska man ha int eller double? TODO
 		int playingfieldHeight;
-		double clickedXCoord;
-		double clickedYCoord;
 		double distance;
-		double distanceX;
-		double distanceY;
 
 		// private void getClickedCoords(Event event) {
-		// } TODO ska man ha funktion för detta eller göra det i
-		// eventhandlern????
+		// } 
+		// TODO funktion för detta 
+
+		Scene scene = new Scene(pane, 450, 550);
 
 		playingfieldWidth = (int) pane.getWidth();
 		playingfieldHeight = (int) pane.getHeight();
+//		createStartPoint();
 
 		pane.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
 			// vid klick: bollen rullar i given riktning med en fart som
 			// bestämts av avståndet mellan startpunkten och klickpunkten
 			// vid klick: boll uppstår på startpunkten och rör sig mot klicket
-			xcoor = event.getX();
-			int xcoorInt = (int) event.getX();
-			ycoor = event.getY();
-			int ycoorInt = (int) event.getY();
-			String utskrift = ("x-koor: " + xcoor + " y-koor: " + ycoor + "\n"
-					+ "x-koorInt: " + xcoorInt + " y-koorInt: " + ycoorInt);
-			resultLabel.setText(utskrift);
-			pane.getChildren().add(new Ball(30, 30, 20, Color.CHARTREUSE,
-					playingfieldWidth, playingfieldHeight));
+			clickedXCoord = event.getX();
+			clickedYCoord = event.getY();
+			startXCoord = playingfieldWidth / 2;
+			startYCoord = playingfieldHeight - 25; // TODO kunna ange storlek på boll
+
+			distanceX = clickedXCoord - startXCoord;
+			distanceY = clickedYCoord - startYCoord;
+
+			ball = new Ball(startXCoord, startYCoord, 20, Color.CHARTREUSE,
+					playingfieldWidth, playingfieldHeight, distanceX/20, distanceY/20);
+			pane.getChildren().add(ball);
+			ball.animateBallMovement();
 
 			// getClickedCoords(event);
 		});
 
-		resultLabel.setStyle("-fx-border-color: yellow");
-		pane.setTop(resultLabel);
-		Scene scene = new Scene(pane, 250, 150);
 		// förberedelse för att kunna ha olika storlekar på spelplanen
-		startXCoord = playingfieldWidth / 2;
-		startYCoord = playingfieldHeight;
-		clickedXCoord = xcoor;
-		clickedYCoord = ycoor;
 
-		MoveTo moveTo = new MoveTo();
-		moveTo.setX(startXCoord);
-		moveTo.setY(startYCoord);
-		LineTo line1 = new LineTo(startXCoord + 2, startYCoord);
-		LineTo line2 = new LineTo(startXCoord, startYCoord - 4);
-		LineTo line3 = new LineTo(startXCoord - 2, startYCoord);
-		path.getElements().addAll(moveTo, line1, line2, line3);
-		pane.getChildren().add(path);
+//		private void createStartPoint(int startXCoord, int startYCoord) {
+//			MoveTo moveTo = new MoveTo();
+//			moveTo.setX(startXCoord);
+//			moveTo.setY(startYCoord);
+//			LineTo line1 = new LineTo(startXCoord + 5, startYCoord);
+//			LineTo line2 = new LineTo(startXCoord, startYCoord - 8);
+//			LineTo line3 = new LineTo(startXCoord - 5, startYCoord);
+//			LineTo line4 = new LineTo(startXCoord, startYCoord);
+//			path.getElements().addAll(moveTo, line1, line2, line3, line4);
+//			pane.getChildren().add(path);
+//		}
+		
 		distance = Math.sqrt(Math.pow((clickedXCoord - startXCoord), 2)
 				+ (Math.pow((clickedYCoord - startYCoord), 2)));
-		distanceX = clickedXCoord - startXCoord;
-		distanceY = clickedYCoord - startYCoord;
-
-		primaryStage.setTitle("Working on fourth stage");
+		primaryStage.setTitle("stage 11 working");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
